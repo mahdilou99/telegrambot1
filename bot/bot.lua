8f6aec7a9ee527a63141e3b42b611b3d58d2b34e
mahdilou99 <mahdilou99@gmail.com>
2016-08-14 20:42:18 +0200

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {our_id},
+    sudo_users = {9399353},
     admin_users = {},
     disabled_channels = {}
   }
